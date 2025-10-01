058b32f5ebc9fadf848a3f74ffc1059407e16a4c
Murtaza Singapurwala <66314258+Murtaza6714@users.noreply.github.com>
2025-10-01 17:16:24 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [
       {
-        name: "skinska-frontend", // Choose a unique name for your app
+        name: "kathiawar-frontend", // Choose a unique name for your app
         script: "node_modules/next/dist/bin/next", // Path to Next.js executable
         args: "start -p 4007", // Command to start Next.js server
         watch: true // Set to true if you want PM2 to watch for file changes and restart the app
